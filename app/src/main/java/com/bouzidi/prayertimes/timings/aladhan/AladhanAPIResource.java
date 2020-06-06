5bf492a94b9ee47f7046040fc3a7320ed9021304
HBouzidi <hicham.bouzidi@gmail.com>
2020-06-06 16:55:47 +0200

    Fix path variable on adhan API

--- app/src/main/java/com/bouzidi/prayertimes/timings/aladhan/AladhanAPIResource.java
+++ app/src/main/java/com/bouzidi/prayertimes/timings/aladhan/AladhanAPIResource.java
@@ -2,19 +2,20 @@ package com.bouzidi.prayertimes.timings.aladhan;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface AladhanAPIResource {
 
-    @GET("timingsByCity")
-    Call<AladhanTodayTimingsResponse> getTimingsByCity(@Query("date_or_timestamp") String date,
+    @GET("timingsByCity/{date_or_timestamp}")
+    Call<AladhanTodayTimingsResponse> getTimingsByCity(@Path("date_or_timestamp") String date,
                                                        @Query("city") String city,
                                                        @Query("country") String country,
                                                        @Query("method") int method);
 
-    @GET("timings")
-    Call<AladhanTodayTimingsResponse> getTimingsByLatLong(@Query("latitude") String latitude,
+    @GET("timings/{date_or_timestamp}")
+    Call<AladhanTodayTimingsResponse> getTimingsByLatLong(@Path("date_or_timestamp") String date,
+                                                          @Query("latitude") String latitude,
                                                           @Query("longitude") Double longitude,
-                                                          @Query("date_or_timestamp") String date,
                                                           @Query("method") int method);
 }

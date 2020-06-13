d361525b615429f98d241f163c907b0330199f95
HBouzidi <hicham.bouzidi@gmail.com>
2020-06-14 00:18:05 +0200

    using media player instead of ringtone manager to prevent looping effect.

--- app/src/main/java/com/bouzidi/prayertimes/notifier/AdhanPlayer.java
+++ app/src/main/java/com/bouzidi/prayertimes/notifier/AdhanPlayer.java
@@ -2,23 +2,26 @@ package com.bouzidi.prayertimes.notifier;
 
 import android.content.ContentResolver;
 import android.content.Context;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.net.Uri;
 
 import com.bouzidi.prayertimes.R;
 
 public class AdhanPlayer {
 
-    public static Ringtone r;
+    public static MediaPlayer mediaPlayer;
 
     public static void playAdhan(Context context) {
         Uri soundUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + context.getPackageName() + "/" + R.raw.adhan_meshary_al_fasy);
-        r = RingtoneManager.getRingtone(context, soundUri);
-        r.play();
+        mediaPlayer = MediaPlayer.create(context, soundUri);
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_NOTIFICATION);
+        mediaPlayer.setLooping(false);
+
+        mediaPlayer.start();
     }
 
     public static void stopAdhan() {
-        r.stop();
+        mediaPlayer.stop();
     }
 }

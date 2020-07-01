4348e331ce4cc20eb606dc5d9f096783b2ddbe9e
HBouzidi <hicham.bouzidi@gmail.com>
2020-07-01 16:54:49 +0200

    improve compass : Add rotation vector sensor type

--- app/src/main/java/com/bouzidi/prayertimes/ui/qibla/Compass.java
+++ app/src/main/java/com/bouzidi/prayertimes/ui/qibla/Compass.java
@@ -11,8 +11,8 @@ import android.hardware.SensorManager;
 import android.util.Log;
 
 /**
- * @author Hassaan Jamil
- * @link https://github.com/hassaanjamil/hj-android-lib-qibla-direction
+ * @author Gokul Swaminathan
+ * @link https://github.com/JavaCafe01/MaterialCompass
  * Updated By Hicham Bouzidi Idrissi
  */
 public class Compass implements SensorEventListener {
@@ -24,9 +24,15 @@ public class Compass implements SensorEventListener {
 
     private CompassListener listener;
 
+    private static final float ROTATION_VECTOR_SMOOTHING_FACTOR = 0.5f;
+    private static final int SENSOR_DELAY_MICROS = 16 * 1000;
+
     private SensorManager sensorManager;
+    private Sensor rsensor;
     private Sensor asensor;
     private Sensor msensor;
+
+    private boolean useRotationVectorSensor = false;
 
     private float[] aData = new float[3];
     private float[] mData = new float[3];
@@ -35,10 +41,12 @@ public class Compass implements SensorEventListener {
 
     private float azimuthFix;
 
-    public Compass(Context context) {
-        sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+    public Compass(Activity activity) {
+        sensorManager = (SensorManager) activity.getSystemService(Context.SENSOR_SERVICE);
+
         asensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         msensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        rsensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
     }
 
     public void start(Context context) {
@@ -46,32 +54,22 @@ public class Compass implements SensorEventListener {
                 SensorManager.SENSOR_DELAY_GAME);
         sensorManager.registerListener(this, msensor,
                 SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener(this, rsensor,
+                SENSOR_DELAY_MICROS);
 
         PackageManager manager = context.getPackageManager();
         boolean haveAS = manager.hasSystemFeature(PackageManager.FEATURE_SENSOR_ACCELEROMETER);
         boolean haveCS = manager.hasSystemFeature(PackageManager.FEATURE_SENSOR_COMPASS);
+        boolean haveCR = manager.hasSystemFeature(PackageManager.FEATURE_SENSOR_GYROSCOPE);
 
-        if (!haveAS || !haveCS) {
+        if ((!haveAS || !haveCS) && !haveCR) {
             sensorManager.unregisterListener(this, asensor);
             sensorManager.unregisterListener(this, msensor);
-            Log.e(TAG, "Device don't have enough sensors");
+            sensorManager.unregisterListener(this, rsensor);
 
+            Log.e(TAG, "Device don't have enough sensors");
             dialogError(context);
         }
-    }
-
-    private void dialogError(final Context context) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setTitle(context.getString(com.bouzidi.prayertimes.R.string.dialog_title));
-        builder.setCancelable(false);
-        builder.setIcon(android.R.drawable.ic_dialog_alert);
-        builder.setMessage(context.getString(com.bouzidi.prayertimes.R.string.dialog_message_sensor_not_exist));
-        builder.setNegativeButton("OK", (dialog, which) -> {
-            dialog.dismiss();
-            if (context instanceof Activity)
-                ((Activity) context).finish();
-        });
-        builder.create().show();
     }
 
     public void stop() {
@@ -92,44 +90,99 @@ public class Compass implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        final float alpha = 0.97f;
-
         synchronized (this) {
-            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-                aData[0] = alpha * aData[0] + (1 - alpha)
-                        * event.values[0];
-                aData[1] = alpha * aData[1] + (1 - alpha)
-                        * event.values[1];
-                aData[2] = alpha * aData[2] + (1 - alpha)
-                        * event.values[2];
-            }
-
-            if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
-                mData[0] = alpha * mData[0] + (1 - alpha)
-                        * event.values[0];
-                mData[1] = alpha * mData[1] + (1 - alpha)
-                        * event.values[1];
-                mData[2] = alpha * mData[2] + (1 - alpha)
-                        * event.values[2];
-            }
-
-            boolean success = SensorManager.getRotationMatrix(R, I, aData, mData);
-            if (success) {
-                float[] orientation = new float[3];
-                SensorManager.getOrientation(R, orientation);
-                float azimuth = (float) Math.toDegrees(orientation[0]); // orientation
-                azimuth = (azimuth + azimuthFix + 360) % 360;
-                //azimuth = (azimuth + azimuthFix + 360) % 294;
-
-                // Log.d(TAG, "azimuth (deg): " + azimuth);
-                if (listener != null) {
-                    listener.onNewAzimuth(azimuth);
-                }
+            if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+                useRotationVectorSensor = true;
+                getAzimuthFromRotationSensor(event);
+            } else if (!useRotationVectorSensor) {
+                getAzimuthFromOtherSensors(event);
             }
         }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    private void getAzimuthFromRotationSensor(SensorEvent event) {
+        float[] mRotationMatrixFromVector = new float[16];
+        float[] adjustedRotationMatrix = new float[16];
+        float[] orientationVals = new float[3];
+        float[] mRotationVector = new float[5];
+
+        mRotationVector = exponentialSmoothing(event.values, mRotationVector, ROTATION_VECTOR_SMOOTHING_FACTOR);
+        SensorManager.getRotationMatrixFromVector(mRotationMatrixFromVector, mRotationVector);
+        SensorManager.remapCoordinateSystem(mRotationMatrixFromVector,
+                SensorManager.AXIS_X, SensorManager.AXIS_Z,
+                adjustedRotationMatrix);
+
+        SensorManager.getOrientation(adjustedRotationMatrix, orientationVals);
+
+        float azimuth = (float) Math.toDegrees(orientationVals[0]);
+        azimuth = (azimuth + azimuthFix + 360) % 360;
+
+        if (listener != null) {
+            listener.onNewAzimuth(azimuth);
+        }
+    }
+
+    private void getAzimuthFromOtherSensors(SensorEvent event) {
+        final float alpha = 0.97f;
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            aData[0] = alpha * aData[0] + (1 - alpha)
+                    * event.values[0];
+            aData[1] = alpha * aData[1] + (1 - alpha)
+                    * event.values[1];
+            aData[2] = alpha * aData[2] + (1 - alpha)
+                    * event.values[2];
+        }
+
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            mData[0] = alpha * mData[0] + (1 - alpha)
+                    * event.values[0];
+            mData[1] = alpha * mData[1] + (1 - alpha)
+                    * event.values[1];
+            mData[2] = alpha * mData[2] + (1 - alpha)
+                    * event.values[2];
+        }
+
+        boolean success = SensorManager.getRotationMatrix(R, I, aData, mData);
+
+        if (success) {
+            float[] orientation = new float[3];
+            SensorManager.getOrientation(R, orientation);
+            float azimuth = (float) Math.toDegrees(orientation[0]);
+            azimuth = (azimuth + azimuthFix + 360) % 360;
+
+            if (listener != null) {
+                listener.onNewAzimuth(azimuth);
+            }
+        }
+    }
+
+    private float[] exponentialSmoothing(float[] newValue, float[] lastValue, float alpha) {
+        float[] output = new float[newValue.length];
+        if (lastValue == null) {
+            return newValue;
+        }
+        for (int i = 0; i < newValue.length; i++) {
+            output[i] = lastValue[i] + alpha * (newValue[i] - lastValue[i]);
+        }
+        return output;
+    }
+
+    private void dialogError(final Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(context.getString(com.bouzidi.prayertimes.R.string.dialog_title));
+        builder.setCancelable(false);
+        builder.setIcon(android.R.drawable.ic_dialog_alert);
+        builder.setMessage(context.getString(com.bouzidi.prayertimes.R.string.dialog_message_sensor_not_exist));
+        builder.setNegativeButton("OK", (dialog, which) -> {
+            dialog.dismiss();
+            if (context instanceof Activity)
+                ((Activity) context).finish();
+        });
+        builder.create().show();
     }
 }

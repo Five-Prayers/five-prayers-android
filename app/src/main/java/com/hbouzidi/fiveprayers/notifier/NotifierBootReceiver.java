e567fdd0cfcf70c5761b39242dc56e44d4d4cacd
HBouzidi <hicham.bouzidi@gmail.com>
2020-10-22 00:04:15 +0200

    Improve handling boot complete event

--- app/src/main/java/com/hbouzidi/fiveprayers/notifier/NotifierBootReceiver.java
+++ app/src/main/java/com/hbouzidi/fiveprayers/notifier/NotifierBootReceiver.java
@@ -6,14 +6,10 @@ import android.content.Intent;
 
 import com.hbouzidi.fiveprayers.job.PeriodicWorkCreator;
 
-import java.util.Objects;
-
 public class NotifierBootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent != null && Objects.equals(intent.getAction(), "android.intent.action.BOOT_COMPLETED")) {
-            PeriodicWorkCreator.schedulePrayerUpdater(context);
-        }
+        PeriodicWorkCreator.schedulePrayerUpdater(context);
     }
 }

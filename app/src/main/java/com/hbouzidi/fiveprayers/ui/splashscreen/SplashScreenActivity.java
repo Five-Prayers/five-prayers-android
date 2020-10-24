b36ee2a0d82a276df24a704ac8084fd5f103d7b2
HBouzidi <hicham.bouzidi@gmail.com>
2020-10-24 13:59:26 +0200

    No need to preload timings, most of time they are retrieved twice from DB.

--- app/src/main/java/com/hbouzidi/fiveprayers/ui/splashscreen/SplashScreenActivity.java
+++ app/src/main/java/com/hbouzidi/fiveprayers/ui/splashscreen/SplashScreenActivity.java
@@ -1,7 +1,6 @@
 package com.hbouzidi.fiveprayers.ui.splashscreen;
 
 import android.animation.ObjectAnimator;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ProgressBar;
@@ -9,23 +8,10 @@ import android.widget.ProgressBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.hbouzidi.fiveprayers.R;
-import com.hbouzidi.fiveprayers.location.address.AddressHelper;
-import com.hbouzidi.fiveprayers.location.tracker.LocationHelper;
-import com.hbouzidi.fiveprayers.timings.DayPrayer;
-import com.hbouzidi.fiveprayers.timings.TimingsService;
-import com.hbouzidi.fiveprayers.timings.TimingServiceFactory;
 import com.hbouzidi.fiveprayers.ui.MainActivity;
-
-import java.time.LocalDate;
-
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.disposables.CompositeDisposable;
-import io.reactivex.rxjava3.observers.DisposableSingleObserver;
-import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
-    private CompositeDisposable compositeDisposable;
     private final static int LOADING_TIME = 1200;
 
     @Override
@@ -37,8 +23,6 @@ public class SplashScreenActivity extends AppCompatActivity {
         ObjectAnimator objectAnimator = ObjectAnimator.ofInt(progressBar, "progress", 100);
         objectAnimator.setDuration(LOADING_TIME);
         objectAnimator.start();
-
-        preloadTodayPrayerTimings(this);
 
         Thread myThread = new Thread() {
             @Override
@@ -56,36 +40,8 @@ public class SplashScreenActivity extends AppCompatActivity {
         myThread.start();
     }
 
-    private void preloadTodayPrayerTimings(Context context) {
-        TimingsService timingsService = TimingServiceFactory.create();
-
-        compositeDisposable = new CompositeDisposable();
-        compositeDisposable.add(
-                LocationHelper.getLocation(context)
-                        .flatMap(location ->
-                                AddressHelper.getAddressFromLocation(location, context)
-                        ).flatMap(address ->
-                        timingsService.getTimingsByCity(
-                                LocalDate.now(),
-                                address,
-                                context
-                        ))
-                        .subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribeWith(new DisposableSingleObserver<DayPrayer>() {
-                            @Override
-                            public void onSuccess(DayPrayer dayPrayer) {
-                            }
-
-                            @Override
-                            public void onError(@io.reactivex.rxjava3.annotations.NonNull Throwable e) {
-                            }
-                        }));
-    }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        compositeDisposable.dispose();
     }
 }

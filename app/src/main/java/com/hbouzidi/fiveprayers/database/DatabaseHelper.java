ab40746b544166058572309ea97bb33da5c48178
HBouzidi <hicham.bouzidi@gmail.com>
2021-02-14 21:50:42 +0100

    Bump version

--- app/src/main/java/com/hbouzidi/fiveprayers/database/DatabaseHelper.java
+++ app/src/main/java/com/hbouzidi/fiveprayers/database/DatabaseHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
     public static final String DATABASE_NAME = "five_prayer.db";
 
     public DatabaseHelper(@Nullable Context context) {

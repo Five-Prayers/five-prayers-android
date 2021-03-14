c8a35f1b62959c160382ca03a57eda76eef72274
HBouzidi <hicham.bouzidi@gmail.com>
2021-03-14 10:21:42 +0100

    Bug Fix : Permission issues on API 21 - Download manager cursor issue

--- app/src/main/java/com/hbouzidi/fiveprayers/ui/quran/index/QuranIndexViewModel.java
+++ app/src/main/java/com/hbouzidi/fiveprayers/ui/quran/index/QuranIndexViewModel.java
@@ -152,6 +152,7 @@ public class QuranIndexViewModel extends AndroidViewModel {
                         }
                     }
                 }
+                cursor.close();
             }
         }).start();
     }

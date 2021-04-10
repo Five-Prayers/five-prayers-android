e7a5343abdcee545645f31034ae2263261334aee
HBouzidi <hicham.bouzidi@gmail.com>
2021-04-10 10:05:11 +0200

    Clean unused imports

--- app/src/main/java/com/hbouzidi/fiveprayers/ui/qibla/CompassActivity.java
+++ app/src/main/java/com/hbouzidi/fiveprayers/ui/qibla/CompassActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.Surface;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
@@ -17,7 +16,6 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
 import androidx.preference.PreferenceManagerFix;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;

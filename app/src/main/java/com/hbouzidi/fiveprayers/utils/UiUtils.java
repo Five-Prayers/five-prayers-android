72853092aeab99f79cb7872c4b9cb9fc49acf870
HBouzidi <hicham.bouzidi@gmail.com>
2021-12-09 20:48:31 +0100

    Fix formatting issues (persian numbers)

--- app/src/main/java/com/hbouzidi/fiveprayers/utils/UiUtils.java
+++ app/src/main/java/com/hbouzidi/fiveprayers/utils/UiUtils.java
@@ -5,11 +5,13 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.net.Uri;
 
+import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DecimalStyle;
 import java.time.format.FormatStyle;
 import java.util.Locale;
 
@@ -32,7 +34,10 @@ public class UiUtils {
     }
 
     public static String formatShortDate(LocalDate localDate) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(GREGORIAN_MONTH_YEAR_FORMAT, Locale.getDefault());
+        DateTimeFormatter formatter = DateTimeFormatter
+                .ofPattern(GREGORIAN_MONTH_YEAR_FORMAT, Locale.getDefault())
+                .withDecimalStyle(DecimalStyle.of(Locale.getDefault()))
+                .withLocale(Locale.getDefault());
         return localDate.format(formatter);
     }
 
@@ -40,31 +45,59 @@ public class UiUtils {
         ZoneId zoneId = ZoneId.systemDefault();
         ZonedDateTime zdt = ZonedDateTime.of(localDateTime, zoneId);
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT).withLocale(Locale.getDefault());
+        DateTimeFormatter formatter = DateTimeFormatter
+                .ofLocalizedTime(FormatStyle.SHORT)
+                .withDecimalStyle(DecimalStyle.of(Locale.getDefault()))
+                .withLocale(Locale.getDefault());
         return zdt.format(formatter);
     }
 
     public static String formatReadableGregorianDate(ZonedDateTime zonedDateTime) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL).withLocale(Locale.getDefault());
-        return zonedDateTime.toLocalDate().format(formatter);
+        DateTimeFormatter formatter = DateTimeFormatter
+                .ofLocalizedDate(FormatStyle.FULL)
+                .withDecimalStyle(DecimalStyle.of(Locale.getDefault()))
+                .withLocale(Locale.getDefault());
+        return zonedDateTime.toLocalDate().format(formatter).replaceAll("[٬،.]", "");
     }
 
     public static String formatReadableGregorianDate(LocalDate localDate) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL).withLocale(Locale.getDefault());
-        return localDate.format(formatter);
+        DateTimeFormatter formatter = DateTimeFormatter
+                .ofLocalizedDate(FormatStyle.FULL)
+                .withDecimalStyle(DecimalStyle.of(Locale.getDefault()))
+                .withLocale(Locale.getDefault());
+        return localDate.format(formatter).replaceAll("[٬،.]", "");
     }
 
     public static String formatReadableTimezone(ZonedDateTime zonedDateTime) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(TIME_ZONE_READABLE_FORMAT, Locale.getDefault());
+        DateTimeFormatter formatter = DateTimeFormatter
+                .ofPattern(TIME_ZONE_READABLE_FORMAT, Locale.getDefault())
+                .withDecimalStyle(DecimalStyle.of(Locale.getDefault()))
+                .withZone(ZoneId.systemDefault())
+                .withLocale(Locale.getDefault());
         return zonedDateTime.format(formatter);
     }
 
+    public static String formatCalculationMethodAngle(String angle) {
+        NumberFormat numberFormat = NumberFormat.getInstance();
+        return numberFormat.format(Float.parseFloat(angle));
+    }
+
+    public static String formatFullHijriDate(String nameOfTheDay, int day, String monthName, int year) {
+        NumberFormat numberFormat = NumberFormat.getInstance();
+        String date = nameOfTheDay + " " + numberFormat.format(day) + " " + monthName + " " + numberFormat.format(year);
+        return date.replaceAll("[٬،.,]", "");
+    }
+
     public static String formatHijriDate(int day, String monthName, int year) {
-        return String.format(Locale.getDefault(), "%1$02d", day) + " " + monthName + " " + year;
+        NumberFormat numberFormat = NumberFormat.getInstance();
+        String date = numberFormat.format(day) + " " + monthName + " " + numberFormat.format(year);
+        return date.replaceAll("[٬،.,]", "");
     }
 
     public static String formatShortHijriDate(int day, String monthName) {
-        return String.format(Locale.getDefault(), "%1$02d", day) + " " + monthName;
+        NumberFormat numberFormat = NumberFormat.getInstance();
+        String date = numberFormat.format(day) + " " + monthName;
+        return date.replaceAll("[٬،.,]", "");
     }
 
     public static Uri uriFromRaw(String name, Context context) {
